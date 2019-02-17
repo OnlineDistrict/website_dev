8272904b1a6eac3121f5de851c711d3080f5829b
Spacekoki <koki.lego@gmail.com>
2019-02-17 20:32:14 +0900

    delete

--- ota-main/ota-temp/domain_admin_menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //ドメイン管理者向けメニュー
-    //ドメイン名の変更，ユーザ追加とか色々と
-?>
-<ul type="disc">
-    <li><a href="../ota-admin/domain/user_list.php">ユーザの一覧</a></li>
-    <li>ユーザの追加</li>
-    <li>区域のリース更新</li>
-</ul>
--- ota-main/ota-temp/territory_admin_menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //ドメイン管理者向けメニュー
-    //ドメイン名の変更，ユーザ追加とか色々と
-?>
-<ul type="disc">
-    <li><a href="../ota-admin/territory/new_territory.php">新しい区域の登録</a></li>
-    <li>ログ</li>
-</ul>
